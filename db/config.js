e05ba52389f080c765b623085ee287ed1751a070
cannotwritecode <openedbakery@gmail.com>
2025-05-24 20:36:45 +0100

    modify configuration fo databse

--- db/config.js
+++ db/config.js
@@ -9,9 +9,13 @@ const { Pool } = require("pg");
 // });
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   ssl: {
-    rejectUnauthorized: false, // for Supabase/Neon
+    rejectUnauthorized: false,
   },
 });
 
